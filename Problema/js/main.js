8b4cd71cd24049dfb1c98490a1a8965fb0722a45
unknown <katherine12>
2016-05-17 19:37:17 -0500

    inicio

---
+++ Problema/js/main.js
@@ -0,0 +1,25 @@
+var imagenes = [
+  "Alma Patricia Jimenez.JPG",  "Ana Maria Barbosa.JPG",
+  "Analy Miranda.JPG",          "Areli Rodriguez.JPG",
+  "Beatriz Quesadas.JPG",       "Claudia Hernández.JPG",
+  "Daniela Belem García.JPG",   "Elisa Guadalupe Martínez.JPG",
+  "Evelyn Vázquez.JPG",         "Gabriela Peñaloza.JPG",
+  "Grissel Coutiño.JPG",        "Guadalupe Guerrero.JPG",
+  "Johana Alexa Vargas.JPG",    "Joyce Zeltzin Hernández.JPG",
+  "Juana Ofelia García.JPG",    "Karen A. Sha ron De Diego.JPG",
+  "Karen Cruz Heredia.JPG",     "Karen Quiroz.JPG",
+  "Karla Monica Llerenas.JPG",  "Karla Vargas.JPG",
+  "Leslie Anigail Vazquez.JPG", "Lilian Mishel Martínez.JPG",
+  "Milca Sarai Del Angel.JPG",  "Naibit Leonel.JPG",
+  "Nayeli Carbajal.JPG",        "Nelly Montserrat Saavedra.JPG",
+  "Reyna Hernández.JPG",        "Rubí Adriana Santiago.JPG",
+  "Ruth López.JPG",             "Ruth Zacnicte Vega.JPG",
+  "Sandra Bollo.JPG",           "Sandra Díaz.JPG",
+  "Vianey Tavatha Moreno.JPG",  "Zazil Aurora Martinez.JPG"
+];
+
+var nombres = [ "Alma", "Ana", "Analy", "Areli", "Beatriz", "Claudia", 
+"Daniela", "Elisa", "Evelyn", "Gabriela", "Grissel", "Guadalupe", "Johana", 
+"Joyce", "Juana", "Karen", "Karen", "Karen", "Karla", "Karla", "Leslie", 
+"Lilian", "Milca", "Naibit", "Nayeli", "Nelly", "Reyna", "Rubi", "Ruth",
+ "Ruth", "Sandra", "Sandra", "Vianey", "Zazil"];
